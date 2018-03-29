5482275ce3ed7037b501fb92d32589a7933078da
hatuw <jiaxi_wu@ieee.org>
2018-03-29 21:38:22 +0800

    beautify code

--- infosec-lab1/lab1-1/main.py
+++ infosec-lab1/lab1-1/main.py
@@ -1,24 +1,42 @@
 # -*- coding:utf-8 -*-
-import os, sys
-sys.path.append('.')
-from AESEncrypt import *
+import os, re
+from MyAES import *
+"""
+Using different mode in AES to encrypt a bmp image.
+But it doesn't support decrypt yet...
+"""
+
+LOG_PATH = './aes.log'
+OUT_DIR = './images/'
+IMG_PATH = './images/lena512.bmp'
+KEY = 'keyskeyskeyskeys'
+MODE_LIST = ['ECB', 'CBC', 'CFB', 'OFB', 'CTR']
 
 
 def test(key, mode):
-    my_aes = AESEncrypt(key, mode)
+    # testing for encrypt/decrypt text
+    my_aes = MyAES(key, mode)
     e = my_aes.encrypt(text='0123456789ABCDEF')
     print(e)
     d = my_aes.decrypt(e)
     print(d)
 
-if __name__ == '__main__':
-    if os.path.isfile('./aes.log'):
-        os.remove('./aes.log')
-    key = 'keyskeyskeyskeys'
-    mode = 'ECB'
-    mode_list = ['ECB', 'CBC', 'CFB', 'OFB', 'CTR']
-    for mode in mode_list:
+
+def encrypt():
+    # Encrypt the bmp image using different modes
+    for mode in MODE_LIST:
         print('[+]{} mode'.format(mode))
-        # test(key, mode)
-        my_aes = AESEncrypt(key, mode, img_clr='./images/lena512.bmp')
-        my_aes.encrypt(img_out='./images/{}_out.bmp'.format(mode))
+        my_aes = MyAES(KEY, mode, img_clr=IMG_PATH)
+        my_aes.encrypt(img_out=os.path.join(OUT_DIR, '{}_out.bmp'.format(mode)))
+
+
+def main():
+    if os.path.isfile(LOG_PATH):
+        os.remove(LOG_PATH)
+    for file in os.listdir(OUT_DIR):
+        if re.match(r'.*_out.bmp', file):
+            os.remove(os.path.join(OUT_DIR, file))
+    encrypt()
+
+if __name__ == '__main__':
+    main()
