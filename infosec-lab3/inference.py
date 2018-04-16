6c0fc636788f17b9171036b9fd577c95e56701c6
hatuw <jiaxi_wu@ieee.org>
2018-04-17 01:21:15 +0800

    init lab3

---
+++ infosec-lab3/inference.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+"""
+Inference of infosec2018-lab3
+Author: Hatuw
+Mail: jiaxi_wu@ieee.org
+"""
+
+# import urllib
+import pandas as pd
+
+SNO = 28    # student no.
+rank_file = './school_list.xlsx'
+
+# load the scholl list
+schools = pd.read_excel(rank_file, [0, 1])
+
+# get the list for processing
+tmp_slice = range((SNO-1)*4, SNO*4)
+domestic_list = schools[0].iloc[tmp_slice, 1].values
+foreign_list = schools[1].iloc[tmp_slice, 1].values
+# print(domestic_list, foreign_list)
